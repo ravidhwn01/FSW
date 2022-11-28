395fff833e3c35bdeb2f9bb1a5d29e1437b49f89
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-11-29 00:27:23 +0530

    database created  using mongoose

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const express = require("express")
+const multer = require("multer");
+require("dotenv").config();
+const File = require("./models/file");
+const mongoose = require("mongoose");
+const express = require("express");
 const app = express();
-const port = 3000
-app.set("view engine", "ejs")
-app.get("/",(req,res)=>{
-    res.render("index")
-} )
+const port = process.env.PORT;
+const upload = multer({ dest: "uploads" });
 
+mongoose.connect(process.env.DATABASE_URL);
 
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-
-
-app.listen(port, ()=>{
-    console.log(`server  connected at port ${port}`)
-})
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname,
+  };
+  if (req.body.password != null && req.body.password != "") {
+    fileData.password = await bcrypt.hash(req.body.password, 10);
+  }
+  const file = await File.create(fileData);
+  console.log(file);
+  res.send(originalName);
+});
+app.listen(process.env.PORT, () => {
+  console.log(`server  connected at port ${port}`);
+});
